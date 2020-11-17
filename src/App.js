1659e6f5c8fff157f600524961ff8007054ad912
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-17 14:48:58 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import { object } from 'prop-types';
+import React, {useState} from 'react';
 import './App.css';
+import {yes, no} from './objects';
+
 function App() {
-  return <h1>You Can Do This!</h1>;
+  const [active, setActive] = useState(false)
+  const [yesObj] = useState(yes)
+  const [noObj] = useState(no)
+
+  const statementMapper = {
+    true: yesObj["yes-statement"],
+    false: noObj["no-statement"]
+  }
+
+  const imageMapper = {
+    true: yesObj["yes-image"],
+    false: noObj["no-image"]
+  }
+
+  
+  return <>
+    <h1>{statementMapper[active.toString()]}</h1>
+    <img onClick={() => setActive(!active)} src={imageMapper[active.toString()]} />
+  </>;
 }
 
 export default App;
